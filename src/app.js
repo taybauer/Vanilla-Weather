a766b70590ab3e528c63166470d1af1e6b9b6aaf
taybauer <102003686+taybauer@users.noreply.github.com>
2022-05-07 18:47:06 -0500

    Moved forecast to JS

--- src/app.js
+++ src/app.js
@@ -21,6 +21,32 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+            src="http://openweathermap.org/img/wn/01d@2x.png"
+            alt=""
+            width="42"
+        />
+        <div class="weather-forecast-temperature">
+            <span class="weather-forecast-temperature-high"> 18° </span>
+            <span class="weather-forecast-temperature-low"> 12° </span>
+        </div>
+    </div>
+    `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -75,6 +101,8 @@ function displayCelsiusTemp(event) {
 }
 
 let celsiusTemp = null;
+
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
